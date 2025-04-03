51f54d22a62f89737df7b6bfa49b211c63133ab2
Bermigal <leeor.b@gmail.com>
2025-04-04 00:03:22 +0300

    Delete components directory

--- components/ui/button.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-export function Button({ children, className = "", ...props }) {
-  return (
-    <button className={`px-4 py-2 rounded-md font-medium transition-all duration-200 ${className}`} {...props}>
-      {children}
-    </button>
-  );
-}
